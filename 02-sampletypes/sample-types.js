383d98093c2ed832998df4c2a72922cc198558e5
tosunismail <ismltsn881@gmail.com>
2024-04-10 03:34:27 +0300

    Added 02-sampletypes

---
+++ 02-sampletypes/sample-types.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.__esModule = true;
+var found = true;
+var grade = 88.6;
+var firstName = "Ismail";
+var lastName = 'Tosun';
+console.log(found);
+console.log("The grade is " + grade);
+console.log("Hi " + firstName + " " + lastName);
---
+++ 02-sampletypes/sample-types.ts
@@ -0,0 +1,11 @@
+export {};
+
+let found: boolean = true;
+let grade: number = 88.6;
+let firstName: string = "Ismail";
+let lastName: string = 'Tosun';
+
+console.log(found);
+console.log("The grade is " + grade);
+
+console.log("Hi " + firstName + " " + lastName);
